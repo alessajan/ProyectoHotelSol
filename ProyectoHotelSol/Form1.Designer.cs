f95c6b1d630126a9421f9248790fa28ea31ffb1d
Alejandra <alesalascast28@gmail.com>
2025-05-07 09:17:52 -0600

    se revirtio merge

--- ProyectoHotelSol/Form1.Designer.cs
+++ ProyectoHotelSol/Form1.Designer.cs
@@ -30,15 +30,15 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.TxtUser = new System.Windows.Forms.TextBox();
-            this.TxtContra = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(97, 47);
+            this.label1.Location = new System.Drawing.Point(71, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 16);
             this.label1.TabIndex = 0;
@@ -47,48 +47,47 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(67, 100);
+            this.label2.Location = new System.Drawing.Point(74, 107);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "PASSWORD:";
             // 
-            // TxtUser
+            // textBox1
             // 
-            this.TxtUser.Location = new System.Drawing.Point(161, 41);
-            this.TxtUser.Name = "TxtUser";
-            this.TxtUser.Size = new System.Drawing.Size(191, 22);
-            this.TxtUser.TabIndex = 2;
+            this.textBox1.Location = new System.Drawing.Point(184, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
             // 
-            // TxtContra
+            // textBox2
             // 
-            this.TxtContra.Location = new System.Drawing.Point(162, 94);
-            this.TxtContra.Name = "TxtContra";
-            this.TxtContra.Size = new System.Drawing.Size(213, 22);
-            this.TxtContra.TabIndex = 3;
+            this.textBox2.Location = new System.Drawing.Point(184, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(180, 151);
+            this.button1.Location = new System.Drawing.Point(144, 173);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "LOGIN";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 237);
+            this.ClientSize = new System.Drawing.Size(374, 268);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.TxtContra);
-            this.Controls.Add(this.TxtUser);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Login";
+            this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,8 +97,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox TxtUser;
-        private System.Windows.Forms.TextBox TxtContra;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
     }
 }
--- ProyectoHotelSol/Form1.cs
+++ ProyectoHotelSol/Form1.cs
@@ -11,38 +11,10 @@ using System.Windows.Forms;
 namespace ProyectoHotelSol
 {
     public partial class Form1 : Form
-
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            using(BDcs mBD = new BDcs())
-            {
-                if (mBD.Conectar())
-                {
-                    string user = TxtUser.Text;
-                    string contra = TxtContra.Text;
-
-                    if(mBD.ValidarUsuario(user, contra))
-                    {
-                        MessageBox.Show("Inicio exitoso");
-
-                    }
-                    else
-                    {
-                        MessageBox.Show("Usuario o contraseña incorrectos");
-                    }
-                    mBD.Desconectar();
-                }
-                else
-                {
-                    MessageBox.Show("No se pudo conectar a la base de datos");
-                }
-            }
         }
     }
 }
